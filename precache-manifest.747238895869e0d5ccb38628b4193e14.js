6be884bc841c2cf377506de0c14c1ea2d2760fe2
Angga Tresna Santoso <atr@voxteneo.asia>
2018-11-21 17:55:12 +0700

    Updates

---
+++ precache-manifest.747238895869e0d5ccb38628b4193e14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0508d00613029b4879d1",
+    "url": "/demo-todo/static/js/runtime~main.0508d006.js"
+  },
+  {
+    "revision": "89dd9cdf5de1107da282",
+    "url": "/demo-todo/static/js/main.89dd9cdf.chunk.js"
+  },
+  {
+    "revision": "086d83fdb50398bb45d7",
+    "url": "/demo-todo/static/js/1.086d83fd.chunk.js"
+  },
+  {
+    "revision": "89dd9cdf5de1107da282",
+    "url": "/demo-todo/static/css/main.4728822a.chunk.css"
+  },
+  {
+    "revision": "aa11444107fbf3910ef14db55f011239",
+    "url": "/demo-todo/index.html"
+  }
+];
